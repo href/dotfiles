9a7bd281a18b1815ab85f8942e9b55594db43af0
Denis Krienbühl <denis@href.ch>
2014-01-30 11:56:18 +0100

    Fix search member function

--- .pdbrc.py
+++ .pdbrc.py
@@ -2,8 +2,8 @@ from __future__ import print_function
 from pprint import pprint
 
 
-def search_member(object, query):
-    results = (m for m in dir(query) if query.lower() in m.lower())
+def search_member(obj, query):
+    results = (m for m in dir(obj) if query.lower() in m.lower())
     map(print, results)
 
 
