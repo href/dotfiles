535521e6fc9c8b92e580bcf35004961fd2685fca
Denis Krienbühl <denis@href.ch>
2014-01-30 11:55:21 +0100

    Fix pdbrc errors

--- .pdbrc.py
+++ .pdbrc.py
@@ -1,10 +1,11 @@
 from __future__ import print_function
-from pprint import pprint as pp
-
-#py flakes
-pp
+from pprint import pprint
 
 
 def search_member(object, query):
     results = (m for m in dir(query) if query.lower() in m.lower())
     map(print, results)
+
+
+sm = search_member
+pp = pprint
