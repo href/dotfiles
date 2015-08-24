3e915f1926b34411a7e2a98f1a772d145c50c2f8
Denis Krienbühl <denis.krienbuehl@seantis.ch>
2015-08-24 11:10:44 +0200

    Adds pdb support for .pdbrc.py

--- .pdbrc.py
+++ .pdbrc.py
@@ -1,6 +1,15 @@
 from __future__ import print_function
 from pprint import pprint
 
+# pdb++ support
+try:
+    from pdb import DefaultConfig
+
+    class Config(DefaultConfig):
+        pass
+except ImportError:
+    pass
+
 
 def search_member(obj, query):
     results = (m for m in dir(obj) if query.lower() in m.lower())
